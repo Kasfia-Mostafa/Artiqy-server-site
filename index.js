d0f66be335809eafbd1374d8ec65b5dc3e0574a8
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2024-04-04 21:37:12 +0600

    server site

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORt || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ijwgr8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+
+    const usersCollection = client.db("ArtiqyDB").collection("users");
+
+
+    //  user related
+    app.post("/users" ,async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email };
+        const existUser = await usersCollection.findOne(query);
+        if (existUser) {
+          return res.send({ message: `user already exist`, insertedId: null });
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+  
+      app.get("/users", async (req, res) => {
+        // console.log(req.headers);
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+      });
+  
+    //   app.delete("/users/:id", async (req, res) => {
+    //     const id = req.params.id;
+    //     const query = { _id: new ObjectId(id) };
+    //     const result = await usersCollection.deleteOne(query);
+    //     res.send(result);
+    //   });
+
+
+
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Artiqy is on work");
+  });
+  
+  app.listen(port, () => {
+    console.log(`Artiqy working on ${port}`);
+  });
