03e31247247ecf910f9d4a6b4ceda82b7b526b25
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2024-04-20 22:43:07 +0600

    login auth

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
-import {  registerUser } from "../Controllers/AuthControllers.js";
-const router = express.Router()
+import { loginUser, registerUser } from "../Controllers/AuthControllers.js";
+const router = express.Router();
 
-router.get('/register', async(req,res)=> {
-    console.log(req)
-    res.send("Register Page")
-})
+// router.get('/register', async(req,res)=> {
+//     console.log(req)
+//     res.send("Register Page")
+// })
 
-router.post('/register', registerUser)
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
-export default router
+export default router;
--- index.js
+++ index.js
@@ -15,22 +15,22 @@ app.use(express.json());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ijwgr8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-    //  {
-    //     useNewUrlParser: true,
-    //     useUnifiedTopology: true,
-    //   }
-  )
-  .then(() => console.log("connected"))
-  .catch((error) => console.log(error));
+const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ijwgr8d.mongodb.net/Artiqy?retryWrites=true&w=majority&appName=Cluster0`;
+(async () => {
+  try {
+    console.log("connecting...");
+    await mongoose.connect(dbUrl);
+    console.log("connected DB");
+  } catch (err) {
+    console.log("connection failed");
+    console.log(err);
+  }
+})();
 
-
-  app.get('/', async(req,res)=> {
-    console.log(req)
-    res.send("Register Page")
-})
+// app.get("/", async (req, res) => {
+//   console.log(req);
+//   res.send("Register Page");
+// });
 
 // usage of routes
 app.use("/auth", AuthRoute);
