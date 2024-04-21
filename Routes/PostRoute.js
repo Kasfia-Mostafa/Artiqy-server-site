70a04a1f821ad62b7a053d79edd14c81f94169c1
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2024-04-21 22:08:47 +0600

    timeline post

--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -5,14 +5,15 @@ import {
   updatePost,
   deletePost,
   likePost,
+  getTimelinePosts,
 } from "../Controllers/PostController.js";
-const router = express.Router();
 
+const router = express.Router();
 
 router.post("/", createPost);
 router.get("/:id", getPost);
 router.put("/:id", updatePost);
 router.delete("/:id", deletePost);
 router.put("/:id/like", likePost);
-// router.get("/:id/timeline", getTimelinePosts);
+router.get("/:id/timeline", getTimelinePosts);
 export default router;
